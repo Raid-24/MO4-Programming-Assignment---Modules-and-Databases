77932f8f156ce129f1dab95e1863c08deb3183c6
Raid-24 <raidangeorge@gmail.com>
2025-11-22 20:23:39 -0500

    Add files via upload

--- zoo.py
+++ zoo.py
@@ -1,3 +1,10 @@
+### Raidan George
+### MO4 Module 4 Programming Assignment - Modules and Databases
+
+### 11.1 - Create a file called zoo.py and in it, define a function called "hours()"
+### that prints the string "Open 9-5 daily". Then, use the interactive interpreter to
+### import the zoo module and call its "hours()" function.
+
 def hours():
     print("Open 9-5 daily")
     return
